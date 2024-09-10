986bdba10e30bde1f7d59b000a1ec293ff9967b7
James Coppock <james.coppock@bionichealth.com>
2024-09-10 14:30:25 -0400

    reinastall

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import Particles from "./components/particles";
+import { GridPattern } from "./components/animated-grid-pattern";
 
 const navigation = [
   { name: "About", href: "/about" },
@@ -24,9 +24,13 @@ export default function Home() {
         </ul>
       </nav>
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-      <Particles
+      <GridPattern
         className="absolute inset-0 -z-10 animate-fade-in"
-        quantity={200}
+        width={40}
+        height={40}
+        numSquares={100}
+        maxOpacity={0.3}
+        duration={4}
       />
       <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-8xl whitespace-nowrap bg-clip-text ">
         James Avery Coppock
@@ -40,5 +44,4 @@ export default function Home() {
       </div>
     </div>
   );
-
 }
